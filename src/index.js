28c7ae432bcbbaf80ee6d7ce06632a4b0ef49d30
Arnar Birgisson <arnarp@gmail.com>
2016-03-02 22:19:14 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Index.js
+if (process.env.NODE_ENV !== 'production') {
+  require('./index.html')
+}
+
+// Accept hot module reloading
+if (module.hot) {
+  module.hot.accept()
+}
+
+require('./index.css') // The page is now styled
+import Please from 'pleasejs'
+const div = document.getElementById('color')
+const button = document.getElementById('button')
+const body = document.getElementsByTagName('body')[0];
+
+const changeColor = () => {
+  div.style.backgroundColor = Please.make_color();
+  body.style.backgroundColor = Please.make_color();
+}
+
+button.addEventListener('click', changeColor);
